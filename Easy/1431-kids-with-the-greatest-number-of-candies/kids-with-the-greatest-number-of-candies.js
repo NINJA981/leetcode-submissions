0cf8e6513b4a6286f1f309363f3065fcc15a6ddd
Meda Venkata Sai Charan <Saicharan312008@gmail.com>
2026-02-07 05:41:11 +0000

    Time: 1 ms (46.78%) | Memory: 54.6 MB (55.34%) - LeetSync

---
+++ Easy/1431-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = Math.max(...candies);
+    var arr=[]
+    for(i=0;i<candies.length;i++){
+        if (candies[i]+extraCandies>=max){
+            arr.push(true)    }
+        else{
+            arr.push(false)
+        }
+    }
+    return arr
+};

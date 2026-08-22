79dea1ae73780b7f948981af77506b6ed217f8f5
Meda Venkata Sai Charan <Saicharan312008@gmail.com>
2026-08-22 23:55:19 +0530

    Time: 1 ms (98.16%) | Memory: 47.3 MB (19.90%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -8,18 +8,13 @@ class Solution {
             if(newInterval[1] < arr[0]){
                 result.add(newInterval);
                 newInterval = arr;
-                
             }
             else if ( newInterval[0] > arr[1]){
                 result.add(arr);
-                
             }
             else{
-
                 newInterval[0] = Math.min(newInterval[0],arr[0]);
                 newInterval[1] = Math.max(newInterval[1],arr[1]);
-
-
             }
         }
         result.add(newInterval);
